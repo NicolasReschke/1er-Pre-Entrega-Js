839b5a7da97686105f1f72a1a4d0b943f62810f4
Nicolas Reschke <nicolas.reschke@hotmail.com>
2023-05-17 00:12:21 -0300

    3er commit

--- js/prestamos.js
+++ js/prestamos.js
@@ -1,9 +1,31 @@
+let monto;
+let cantidadCuotas;
+let montoAdevolver;
+let montoCuota;
+let interes;
+let nombre;
+
+const validarNombre = () => {
+    let nombre = prompt("Para una atención mas personalizada, ingrese su nombre:").toUpperCase();
+    console.log(nombre);
+    while ((nombre === "") || (!/^[a-zA-Z]+$/.test(nombre))) {
+        nombre = prompt("Ingrese correctamente su nombre:").toUpperCase();
+    }
+    console.log(nombre);
+    return nombre;
+}
+
+nombre = validarNombre()
+alert("Bienvenido " + nombre + "!!!\n\n" + "A continuación le informamos el interes según la cantidad de cuotas que puede solicitar: \n"
++ "Hasta 6 cuotas: 20% \n"
++ "Entre 7 y 12 cuotas: 40% \n"
++ "Entre 13 y 24 cuotas: 60% \n"
++ "Entre 25 y 48 cuotas: 80% \n"
++ "Más de 48 cuotas: 100%");
+
+
+// const validarEdad
 let edad = parseFloat(prompt("Ingrese su edad:"));
-let monto = 0;
-let cantidadCuotas = 0;
-let montoAdevolver = 0;
-let montoCuota = 0;
-let interes = 0;
 
 if (edad < 18) {
     alert("Eres menor de edad, no puedes obtener un prestamo!");
@@ -14,7 +36,7 @@ if (edad < 18) {
     let cantidadCuotas = parseFloat(prompt("En cuantas cuotas quiere devolverlo?"));
     
         if (cantidadCuotas <= 6) {
-            interes = 1.5;
+            interes = 1.2;
             montoCuota = ((monto * interes) / cantidadCuotas);
             console.log(monto);
             console.log(cantidadCuotas);
@@ -24,7 +46,7 @@ if (edad < 18) {
             + "\n" + "Ud. devolverá un total de: $" + (montoCuota * cantidadCuotas));
 
         } else if (cantidadCuotas > 6 && cantidadCuotas <=12) {
-            interes = 2;
+            interes = 1.4;
             montoCuota = ((monto * interes) / cantidadCuotas);
             console.log(monto);
             console.log(cantidadCuotas);
@@ -34,7 +56,7 @@ if (edad < 18) {
             + "\n" + "Ud. devolverá un total de: $" + (montoCuota * cantidadCuotas));
 
         } else if (cantidadCuotas > 12 && cantidadCuotas <=24) {
-            interes = 3;
+            interes = 1.6;
             montoCuota = ((monto * interes) / cantidadCuotas);
             console.log(monto);
             console.log(cantidadCuotas);
@@ -44,7 +66,7 @@ if (edad < 18) {
             + "\n" + "Ud. devolverá un total de: $" + (montoCuota * cantidadCuotas));
 
         } else if (cantidadCuotas > 24 && cantidadCuotas <=48) {
-            interes = 4;
+            interes = 1.8;
             montoCuota = ((monto * interes) / cantidadCuotas);
             console.log(monto);
             console.log(cantidadCuotas);
@@ -54,7 +76,7 @@ if (edad < 18) {
             + "\n" + "Ud. devolverá un total de: $" + (montoCuota * cantidadCuotas));
 
         } else if (cantidadCuotas > 48) {
-            interes = 5;
+            interes = 2;
             montoCuota = ((monto * interes) / cantidadCuotas);
             console.log(monto);
             console.log(cantidadCuotas);
